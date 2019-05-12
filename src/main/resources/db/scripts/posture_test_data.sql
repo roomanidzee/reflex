af6be748ea12654ad05038957cf1248205b9bffc
Daniil <doriboty@gmail.com>
2019-05-13 00:45:48 +0300

    test data scripts fix

--- src/main/resources/db/scripts/posture_test_data.sql
+++ src/main/resources/db/scripts/posture_test_data.sql
@@ -1,123 +1,33 @@
 INSERT INTO
     posture_data (smooth_num, flex_num, user_id, date)
 VALUES
-(
-    61, 39, 2, to_date('2019/04/1', 'YYYY/MM/DD')
-)
-        ,
-(
-    97, 3, 2, to_date('2019/04/2', 'YYYY/MM/DD')
-)
-        ,
-(
-    37, 63, 2, to_date('2019/04/3', 'YYYY/MM/DD')
-)
-        ,
-(
-    11, 89, 2, to_date('2019/04/4', 'YYYY/MM/DD')
-)
-        ,
-(
-    53, 47, 2, to_date('2019/04/5', 'YYYY/MM/DD')
-)
-        ,
-(
-    72, 28, 2, to_date('2019/04/6', 'YYYY/MM/DD')
-)
-        ,
-(
-    66, 34, 2, to_date('2019/04/7', 'YYYY/MM/DD')
-)
-        ,
-(
-    9, 91, 2, to_date('2019/04/8', 'YYYY/MM/DD')
-)
-        ,
-(
-    18, 82, 2, to_date('2019/04/9', 'YYYY/MM/DD')
-)
-        ,
-(
-    46, 54, 2, to_date('2019/04/10', 'YYYY/MM/DD')
-)
-        ,
-(
-    99, 1, 2, to_date('2019/04/11', 'YYYY/MM/DD')
-)
-        ,
-(
-    64, 36, 2, to_date('2019/04/12', 'YYYY/MM/DD')
-)
-        ,
-(
-    96, 4, 2, to_date('2019/04/13', 'YYYY/MM/DD')
-)
-        ,
-(
-    17, 83, 2, to_date('2019/04/14', 'YYYY/MM/DD')
-)
-        ,
-(
-    16, 84, 2, to_date('2019/04/15', 'YYYY/MM/DD')
-)
-        ,
-(
-    16, 84, 2, to_date('2019/04/16', 'YYYY/MM/DD')
-)
-        ,
-(
-    19, 81, 2, to_date('2019/04/17', 'YYYY/MM/DD')
-)
-        ,
-(
-    85, 15, 2, to_date('2019/04/18', 'YYYY/MM/DD')
-)
-        ,
-(
-    50, 50, 2, to_date('2019/04/19', 'YYYY/MM/DD')
-)
-        ,
-(
-    45, 55, 1, to_date('2019/04/20', 'YYYY/MM/DD')
-)
-        ,
-(
-    82, 18, 2, to_date('2019/04/21', 'YYYY/MM/DD')
-)
-        ,
-(
-    12, 88, 2, to_date('2019/04/22', 'YYYY/MM/DD')
-)
-        ,
-(
-    44, 56, 2, to_date('2019/04/23', 'YYYY/MM/DD')
-)
-        ,
-(
-    27, 73, 2, to_date('2019/04/24', 'YYYY/MM/DD')
-)
-        ,
-(
-    76, 24, 2, to_date('2019/04/25', 'YYYY/MM/DD')
-)
-        ,
-(
-    5, 95, 2, to_date('2019/04/26', 'YYYY/MM/DD')
-)
-        ,
-(
-    74, 26, 2, to_date('2019/04/27', 'YYYY/MM/DD')
-)
-        ,
-(
-    32, 68, 2, to_date('2019/04/28', 'YYYY/MM/DD')
-)
-        ,
-(
-    70, 30, 2, to_date('2019/04/29', 'YYYY/MM/DD')
-)
-        ,
-(
-    49, 51, 2, to_date('2019/04/30', 'YYYY/MM/DD')
-)
-;
+(2, 48, 2, to_date('2019/04/14', 'YYYY/MM/DD')), 
+(3, 47, 2, to_date('2019/04/15', 'YYYY/MM/DD')), 
+(3, 47, 2, to_date('2019/04/16', 'YYYY/MM/DD')), 
+(4, 46, 2, to_date('2019/04/17', 'YYYY/MM/DD')), 
+(4, 46, 2, to_date('2019/04/18', 'YYYY/MM/DD')), 
+(6, 44, 2, to_date('2019/04/19', 'YYYY/MM/DD')), 
+(7, 43, 2, to_date('2019/04/20', 'YYYY/MM/DD')), 
+(7, 43, 2, to_date('2019/04/21', 'YYYY/MM/DD')), 
+(6, 44, 2, to_date('2019/04/22', 'YYYY/MM/DD')), 
+(8, 42, 2, to_date('2019/04/23', 'YYYY/MM/DD')), 
+(8, 42, 2, to_date('2019/04/24', 'YYYY/MM/DD')), 
+(9, 41, 2, to_date('2019/04/25', 'YYYY/MM/DD')), 
+(12, 38, 2, to_date('2019/04/26', 'YYYY/MM/DD')), 
+(12, 38, 2, to_date('2019/04/27', 'YYYY/MM/DD')), 
+(10, 40, 2, to_date('2019/04/28', 'YYYY/MM/DD')), 
+(10, 40, 2, to_date('2019/04/29', 'YYYY/MM/DD')), 
+(11, 39, 2, to_date('2019/04/30', 'YYYY/MM/DD')),
+(11, 39, 2, to_date('2019/05/1', 'YYYY/MM/DD')), 
+(9, 41, 2, to_date('2019/05/2', 'YYYY/MM/DD')), 
+(10, 40, 2, to_date('2019/05/3', 'YYYY/MM/DD')), 
+(14, 36, 2, to_date('2019/05/4', 'YYYY/MM/DD')), 
+(18, 32, 2, to_date('2019/05/5', 'YYYY/MM/DD')), 
+(22, 28, 2, to_date('2019/05/6', 'YYYY/MM/DD')), 
+(24, 26, 2, to_date('2019/05/7', 'YYYY/MM/DD')), 
+(24, 26, 2, to_date('2019/05/8', 'YYYY/MM/DD')), 
+(22, 28, 2, to_date('2019/05/9', 'YYYY/MM/DD')), 
+(19, 31, 2, to_date('2019/05/10', 'YYYY/MM/DD')), 
+(19, 31, 2, to_date('2019/05/11', 'YYYY/MM/DD')), 
+(21, 21, 2, to_date('2019/05/12', 'YYYY/MM/DD')), 
+(23, 27, 2, to_date('2019/05/13', 'YYYY/MM/DD'));
--- src/main/resources/db/scripts/tiredness_test_data.sql
+++ src/main/resources/db/scripts/tiredness_test_data.sql
@@ -1,123 +1,33 @@
 INSERT INTO
     tiredness_data (morning_val, evening_val, user_id, date)
 VALUES
-(
-    2, 3, 2, to_date('2019/04/1', 'YYYY/MM/DD')
-)
-        ,
-(
-    1, 2, 2, to_date('2019/04/2', 'YYYY/MM/DD')
-)
-        ,
-(
-    4, 4, 2, to_date('2019/04/3', 'YYYY/MM/DD')
-)
-        ,
-(
-    3, 7, 2, to_date('2019/04/4', 'YYYY/MM/DD')
-)
-        ,
-(
-    1, 1, 2, to_date('2019/04/5', 'YYYY/MM/DD')
-)
-        ,
-(
-    2, 6, 2, to_date('2019/04/6', 'YYYY/MM/DD')
-)
-        ,
-(
-    4, 1, 2, to_date('2019/04/7', 'YYYY/MM/DD')
-)
-        ,
-(
-    1, 4, 2, to_date('2019/04/8', 'YYYY/MM/DD')
-)
-        ,
-(
-    5, 5, 2, to_date('2019/04/9', 'YYYY/MM/DD')
-)
-        ,
-(
-    4, 6, 2, to_date('2019/04/10', 'YYYY/MM/DD')
-)
-        ,
-(
-    3, 2, 2, to_date('2019/04/11', 'YYYY/MM/DD')
-)
-        ,
-(
-    5, 7, 2, to_date('2019/04/12', 'YYYY/MM/DD')
-)
-        ,
-(
-    5, 6, 2, to_date('2019/04/13', 'YYYY/MM/DD')
-)
-        ,
-(
-    2, 6, 2, to_date('2019/04/14', 'YYYY/MM/DD')
-)
-        ,
-(
-    6, 4, 2, to_date('2019/04/15', 'YYYY/MM/DD')
-)
-        ,
-(
-    6, 8, 2, to_date('2019/04/16', 'YYYY/MM/DD')
-)
-        ,
-(
-    3, 5, 2, to_date('2019/04/17', 'YYYY/MM/DD')
-)
-        ,
-(
-    2, 4, 2, to_date('2019/04/18', 'YYYY/MM/DD')
-)
-        ,
-(
-    5, 5, 2, to_date('2019/04/19', 'YYYY/MM/DD')
-)
-        ,
-(
-    3, 8, 2, to_date('2019/04/20', 'YYYY/MM/DD')
-)
-        ,
-(
-    7, 6, 2, to_date('2019/04/21', 'YYYY/MM/DD')
-)
-        ,
-(
-    6, 4, 2, to_date('2019/04/22', 'YYYY/MM/DD')
-)
-        ,
-(
-    2, 2, 2, to_date('2019/04/23', 'YYYY/MM/DD')
-)
-        ,
-(
-    7, 9, 2, to_date('2019/04/24', 'YYYY/MM/DD')
-)
-        ,
-(
-    6, 7, 2, to_date('2019/04/25', 'YYYY/MM/DD')
-)
-        ,
-(
-    7, 9, 2, to_date('2019/04/26', 'YYYY/MM/DD')
-)
-        ,
-(
-    3, 4, 2, to_date('2019/04/27', 'YYYY/MM/DD')
-)
-        ,
-(
-    3, 4, 2, to_date('2019/04/28', 'YYYY/MM/DD')
-)
-        ,
-(
-    1, 2, 2, to_date('2019/04/29', 'YYYY/MM/DD')
-)
-        ,
-(
-    3, 1, 2, to_date('2019/04/30', 'YYYY/MM/DD')
-)
-;
+(3, 3, 2, to_date('2019/04/14', 'YYYY/MM/DD')), 
+(3, 4, 2, to_date('2019/04/15', 'YYYY/MM/DD')), 
+(4, 4, 2, to_date('2019/04/16', 'YYYY/MM/DD')), 
+(4, 4, 2, to_date('2019/04/17', 'YYYY/MM/DD')), 
+(4, 5, 2, to_date('2019/04/18', 'YYYY/MM/DD')), 
+(4, 4, 2, to_date('2019/04/19', 'YYYY/MM/DD')), 
+(3, 3, 2, to_date('2019/04/20', 'YYYY/MM/DD')), 
+(4, 3, 2, to_date('2019/04/21', 'YYYY/MM/DD')), 
+(3, 4, 2, to_date('2019/04/22', 'YYYY/MM/DD')), 
+(3, 4, 2, to_date('2019/04/23', 'YYYY/MM/DD')), 
+(4, 4, 2, to_date('2019/04/24', 'YYYY/MM/DD')), 
+(4, 4, 2, to_date('2019/04/25', 'YYYY/MM/DD')), 
+(3, 4, 2, to_date('2019/04/26', 'YYYY/MM/DD')), 
+(3, 3, 2, to_date('2019/04/27', 'YYYY/MM/DD')), 
+(3, 3, 2, to_date('2019/04/28', 'YYYY/MM/DD')), 
+(3, 2, 2, to_date('2019/04/29', 'YYYY/MM/DD')), 
+(3, 3, 2, to_date('2019/04/30', 'YYYY/MM/DD')),
+(2, 3, 2, to_date('2019/05/1', 'YYYY/MM/DD')), 
+(3, 3, 2, to_date('2019/05/2', 'YYYY/MM/DD')), 
+(2, 3, 2, to_date('2019/05/3', 'YYYY/MM/DD')), 
+(2, 3, 2, to_date('2019/05/4', 'YYYY/MM/DD')), 
+(3, 2, 2, to_date('2019/05/5', 'YYYY/MM/DD')), 
+(3, 3, 2, to_date('2019/05/6', 'YYYY/MM/DD')), 
+(3, 3, 2, to_date('2019/05/7', 'YYYY/MM/DD')), 
+(2, 3, 2, to_date('2019/05/8', 'YYYY/MM/DD')), 
+(2, 3, 2, to_date('2019/05/9', 'YYYY/MM/DD')), 
+(2, 2, 2, to_date('2019/05/10', 'YYYY/MM/DD')), 
+(2, 3, 2, to_date('2019/05/11', 'YYYY/MM/DD')), 
+(1, 2, 2, to_date('2019/05/12', 'YYYY/MM/DD')), 
+(2, 2, 2, to_date('2019/05/13', 'YYYY/MM/DD'));
